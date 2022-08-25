e74dbffada0158414c0e459a273abf1c8ffbbdb1
Vinicio Miranda Covalski <vinicio.covalski@gmail.com>
2022-08-24 22:39:38 -0300

    Adicioando o pacote JSOUP, SWAGGER, MODELMAPPER e as classes do pacote util

---
+++ src/main/java/br/com/webscrapjava/brasileiraoapi/util/ScrapingUtil.java
@@ -0,0 +1,10 @@
+package br.com.webscrapjava.brasileiraoapi.util;
+
+public class ScrapingUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/br/com/webscrapjava/brasileiraoapi/util/StatusPartida.java
@@ -0,0 +1,9 @@
+package br.com.webscrapjava.brasileiraoapi.util;
+
+public enum StatusPartida {
+
+	PARTIDA_NAO_INICIADA,
+	PARTIDA_EM_ANDAMENTO,
+	PARTIDA_ENCERRADA,
+	PARTIDA_PENATIS;
+}

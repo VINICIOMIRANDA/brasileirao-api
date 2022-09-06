1b7c4ae97d29e839a04cfab6c7aef524fe0b3bc6
Vinicio Miranda Covalski <vinicio.covalski@gmail.com>
2022-09-05 22:21:44 -0300

    Metodos POST e PUT inseridos

--- src/main/java/br/com/webscrapjava/brasileiraoapi/controller/EquipeController.java
+++ src/main/java/br/com/webscrapjava/brasileiraoapi/controller/EquipeController.java
@@ -1,12 +1,21 @@
 package br.com.webscrapjava.brasileiraoapi.controller;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.webscrapjava.brasileiraoapi.dto.EquipeDTO;
 import br.com.webscrapjava.brasileiraoapi.dto.EquipeResponseDTO;
 import br.com.webscrapjava.brasileiraoapi.entity.Equipe;
 import br.com.webscrapjava.brasileiraoapi.exception.StandardError;
@@ -52,5 +61,41 @@ public class EquipeController {
 	public  ResponseEntity<EquipeResponseDTO> listarEquipes(){
 		return ResponseEntity.ok().body(equipeService.listarEquipes());
 	}
+	
+	@ApiOperation(value = "Inserir equipe")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "OK", response = Equipe.class),
+			@ApiResponse(code = 400, message = "Bad request", response = StandardError.class),
+			@ApiResponse(code = 401, message = "Unauthorized", response = StandardError.class),
+			@ApiResponse(code = 403, message = "Forbidden", response = StandardError.class),
+			@ApiResponse(code = 404, message = "Not Found", response = StandardError.class),
+			@ApiResponse(code = 500, message = "Internal server error", response = StandardError.class)
+	})	
+	@PostMapping()
+	public  ResponseEntity<Equipe> inserirEquipe(@Valid @RequestBody EquipeDTO dto){
+		Equipe equipe = equipeService.inserirEquipe(dto);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(equipe.getId()).toUri();
+		return ResponseEntity.created(location).body(equipe);
+		
+	}
+	
+	@ApiOperation(value = "Alterar equipe")
+	@ApiResponses(value = {
+			@ApiResponse(code = 204, message = "no content", response = Void.class),
+			@ApiResponse(code = 400, message = "Bad request", response = StandardError.class),
+			@ApiResponse(code = 401, message = "Unauthorized", response = StandardError.class),
+			@ApiResponse(code = 403, message = "Forbidden", response = StandardError.class),
+			@ApiResponse(code = 404, message = "Not Found", response = StandardError.class),
+			@ApiResponse(code = 500, message = "Internal server error", response = StandardError.class)
+	})
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> alterarEquipe(@PathVariable("id")Long id, @Valid @RequestBody EquipeDTO dto ){
+		equipeService.alterarEquipe(id, dto);
+			
+		return ResponseEntity.noContent().build();
+		
+	}
+
 
 }
--- src/main/java/br/com/webscrapjava/brasileiraoapi/service/EquipeService.java
+++ src/main/java/br/com/webscrapjava/brasileiraoapi/service/EquipeService.java
@@ -1,12 +1,15 @@
 package br.com.webscrapjava.brasileiraoapi.service;
 
-import java.util.List;
+import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.webscrapjava.brasileiraoapi.dto.EquipeDTO;
 import br.com.webscrapjava.brasileiraoapi.dto.EquipeResponseDTO;
 import br.com.webscrapjava.brasileiraoapi.entity.Equipe;
+import br.com.webscrapjava.brasileiraoapi.exception.BadRequestException;
 import br.com.webscrapjava.brasileiraoapi.exception.NotFoundException;
 import br.com.webscrapjava.brasileiraoapi.repository.EquipeRepository;
 
@@ -16,15 +19,45 @@ public class EquipeService {
 	@Autowired
 	private EquipeRepository equipeRepository;
 
+	@Autowired
+	private ModelMapper modelMapper;
+
 	public Equipe buscarEquipeId(Long id) {
 		return equipeRepository.findById(id)
 				.orElseThrow(() -> new NotFoundException("Nenhuma equipe encontrada com o id informado: " + id));
+	}
+	
+	public Equipe buscarEquipePorNome(String nomeEquipe) {
+		return equipeRepository.findByNomeEquipe(nomeEquipe)
+				.orElseThrow(() -> new NotFoundException("Nenhuma equipe encontrada com o nome informado: " + nomeEquipe));
 	}
 
 	public EquipeResponseDTO listarEquipes() {
 		EquipeResponseDTO equipes = new EquipeResponseDTO();
 		equipes.setEquipes(equipeRepository.findAll());
 		return equipes;
+	}
+
+	public Equipe inserirEquipe(@Valid EquipeDTO dto) {
+		boolean exists = equipeRepository.existsByNomeEquipe(dto.getNomeEquipe());
+		if (exists) {
+			throw new BadRequestException("Já existe uma equipe cadastrada com o nome informado.");
+		}
+
+		Equipe equipe = modelMapper.map(dto, Equipe.class);
+		return equipeRepository.save(equipe);
+	}
+
+	public void alterarEquipe(Long id, EquipeDTO dto) {
+		boolean exists = equipeRepository.existsById(id);
+		
+		if (!exists) {
+			throw new BadRequestException("Não foi possível alterar a equipe: ID inexistente");
+		}
+		Equipe equipe = modelMapper.map(dto, Equipe.class);
+		equipe.setId(id);
+		equipeRepository.save(equipe);
+		
 	}
 
 }

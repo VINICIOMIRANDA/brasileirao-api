9c33ed338e1f7d7e148d10f0921e8ebfb12d5de9
Vinicio Miranda Covalski <vinicio.covalski@gmail.com>
2022-09-07 22:36:20 -0300

    Ajustes no repository partida

--- src/main/java/br/com/webscrapjava/brasileiraoapi/repository/PartidaRepository.java
+++ src/main/java/br/com/webscrapjava/brasileiraoapi/repository/PartidaRepository.java
@@ -11,13 +11,13 @@ import br.com.webscrapjava.brasileiraoapi.entity.Partida;
 @Repository
 public interface PartidaRepository extends JpaRepository<Partida, Long> {
 
-	@Query(name = "buscar_quantidade_partidas_periodo", value = "select count(*) from partidas as p "
-			+ "where p.data_hora_partida between dateadd(hour, -3, current_timestamp) " + "and current_timestamp"
+	@Query(name = "buscar_quantidade_partidas_periodo", value = "select count(*) from partida as p "
+			+ "where p.data_hora_partida between dateadd(hour, -3, current_timestamp) " + "and current_timestamp "
 			+ "and ifnull(p.tempo_partida, 'Vazio') != 'Encerrado' ", nativeQuery = true)
 	public Integer buscarQuantidadePartidasPeriodo();
 
-	@Query(name = "listar_partidas_periodo", value = "select * from partida as p"
-			+ "where p.data_hora_partida between  dateadd(hour, -3, current_timestamp)" + " and current_timestamp"
+	@Query(name = "listar_partidas_periodo", value = "select * from partida as p "
+			+ "where p.data_hora_partida between  dateadd(hour, -3, current_timestamp)" + " and current_timestamp "
 			+ "and ifnull(p.tempo_partida, 'Vazio') != 'Encerrado' ", nativeQuery = true)
 	public List<Partida> listarPartidasPeriodo();
 
